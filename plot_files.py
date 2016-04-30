c806160e7288a2bd27a429277b9f2d5a8c0671e6
Matt Nottingham <matt.nottingham@zen.co.uk>
2016-04-30 16:25:45 +0100

    Add a simple plot program.

---
+++ plot_files.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import numpy as np
+import sys
+
+from matplotlib import pyplot as plt
+
+import cPickle
+
+fp = open(sys.argv[1], 'rb')
+data = cPickle.load(fp)
+fp.close()
+
+ax = plt.axes()
+plt.plot(data['Latest']['freqs'], data['Mean']['data'], label='data - cal')
+plt.plot(data['Latest']['freqs'], data['Cal Data']['data'], label='Raw cal')
+ax.grid()
+plt.xlabel('Freq ('+data['Latest']['freq_units']+')')
+plt.ylabel('Amplitude')
+plt.legend()
+plt.show()
+
+
